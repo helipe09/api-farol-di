107c76aab1b50afc9485377540f588270af45e5d
helipe09 <37268964+helipe09@users.noreply.github.com>
2022-06-01 04:14:45 -0300

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   var cors = require('cors');
-  app.enableCors()
-  app.use(cookieParser(), cors({ credentials: true, origin: 'https://farol-di.web.app' }))
+  app.use(cookieParser(), cors({ credentials: true, origin: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', }))
 
   const configService = app.get(ConfigService);
   config.update({
