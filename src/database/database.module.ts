aaae9dc53b3350c8c6f4851e3684443f5bb76b4c
helipe09 <37268964+helipe09@users.noreply.github.com>
2022-09-01 16:36:06 -0300

    ajsutando

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,7 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
                 type: "postgres",
-                host: configService.get('POSTGRES_HOST') || 'http://ec2-3-14-148-24.us-east-2.compute.amazonaws.com',
+                host: configService.get('POSTGRES_HOST') || 'pgsql',
                 port: configService.get('POSTGRES_PORT') || 5432,
                 username: configService.get('POSTGRES_USER') || 'admin',
                 password: configService.get('POSTGRES_PASSWORD') || 'admin',
